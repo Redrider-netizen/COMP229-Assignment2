ab445cd6f3145fca38889efa643ee88f92fcc684
Redrider-netizen <sahayam.caalvin@gmail.com>
2026-03-20 19:26:46 -0400

    update the backend

--- Backend/Controllers/project.controller.js
+++ Backend/Controllers/project.controller.js
@@ -1,4 +1,4 @@
-const Project = require("../models/project.model");
+const Project = require("../../models/project.model");
 
 // GET ALL
 exports.getAll = async (req, res, next) => {
--- Backend/Controllers/user.controller.js
+++ Backend/Controllers/user.controller.js
@@ -1,4 +1,4 @@
-const User = require("../models/user.model");
+const User = require("../../models/user.model");
 
 // GET ALL
 exports.getAll = async (req, res, next) => {
