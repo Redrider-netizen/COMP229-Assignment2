c23abbcfa5df42c4ef7a1a20d52cf1af8060ae78
Redrider-netizen <sahayam.caalvin@gmail.com>
2026-04-09 18:49:43 -0400

    update api

--- frontend/src/services/api.js
+++ frontend/src/services/api.js
@@ -1,6 +1,8 @@
 import axios from 'axios';
 
-const BASE_URL = process.env.REACT_APP_API_URL || 'http://localhost:3001'; // Backend API URL
+const BASE_URL =
+  process.env.REACT_APP_API_URL ||
+  (process.env.NODE_ENV === 'production' ? '' : 'http://localhost:3001');
 
 const api = axios.create({
   baseURL: BASE_URL,
